c3b9c1138ebe795a3b2169977b752d2683bbba9f
JaeYoun Kim <rlawodusx@naver.com>
2024-08-15 19:24:07 +0900

    Feat: 제네릭 클래스

--- Program.cs
+++ Program.cs
@@ -1,21 +1,32 @@
 ﻿using System;
 
 
-/*
-Event Handler
-*/
+// // 발행자 클래스 
+
+// /*
+// 출력하고 싶은 자료형 마다 메서드 오버로딩을 구현해주어야 한다. 
+// 반복작업을 줄이기 위해 제네릭 타입을 사용 해야 한다. 
 
 
-delegate void myEventHandler();
-// 발행자 클래스 
-class MainClass
-{
+// */
+// class MainClass
+// {
 
-    public static void Main(string[] args)
-    {
-        Publisher publisher = new Publisher();
-        SubScriber sub = new SubScriber();
-        publisher.RunEvent();
-    }
-}
+//     public static void Main(string[] args)
+//     {
+//         int[] intValues = { 1, 2, 3 };
+//         double[] doubleValues = { 1.1, 2.2, 3.3 };
+
+//         PrintValue <int> (intValues);
+//         PrintValue <double> (doubleValues);
+//     }
+
+//     static void PrintValue<T>(T[] values)
+//     {
+//         foreach (var i in values)
+//         {
+//             Console.WriteLine(i);
+//         }
+//     }
+// }
 
--- Test.cs
+++ Test.cs
@@ -1,28 +1,26 @@
-/*
-- 대리자와 이벤트 
-*/
+// /*
+// - 대리자와 이벤트 
+// */
 
+// class SubScriber
+// {
+//     public SubScriber()
+//     {
+//         Publisher.myEvent += TestMethod;
+//     }
 
+//     public void TestMethod()
+//     {
+//         Console.WriteLine("이벤트 메서드 호출!!");
+//     }
+// }
 
-class SubScriber
-{
-    public SubScriber()
-    {
-        Publisher.myEvent += TestMethod;
-    }
+// class Publisher
+// {
+//     public static event myEventHandler myEvent;
 
-    public void TestMethod()
-    {
-        Console.WriteLine("이벤트 메서드 호출!!");
-    }
-}
-
-class Publisher
-{
-    public static event myEventHandler myEvent;
-
-    public void RunEvent()
-    {
-        myEvent();
-    }
-}
+//     public void RunEvent()
+//     {
+//         myEvent();
+//     }
+// }
