ad48f2d5a1b33c8f0a203a398f6adb8921c70e5a
JaeYoun Kim <rlawodusx@naver.com>
2024-08-16 01:10:24 +0900

    Feat: 람다, dynamic, 매개변수 지정

---
+++ Source10_09.cs
@@ -0,0 +1,24 @@
+// /*
+// 람다식
+// 매개변수 => 식 
+// 2개의 매개변수와 코드 블록
+// 매개변수 A, 매개변수 B => { 코드 블록}
+// */
+
+// using System;
+// using System.Collections;
+
+// // delegate int Add(int a, int b);
+
+// class MainClass
+// {
+//     public static void Main(string[] args)
+//     {
+//         dynamic variable = "Hello";
+//         Console.WriteLine(variable);
+
+//         variable = 123;
+//         Console.WriteLine(variable);
+
+//     }
+// }
---
+++ Source10_16.cs
@@ -0,0 +1,40 @@
+using System;
+
+/*
+명명된 매개변수와 선택적 매개변수 
+매개변수는 일반적으로 순서에 따라 결정 
+*/
+
+class Person
+{
+    private string name;
+    private int age;
+    private string gender;
+
+    public Person(string name, int age, string gender)
+    {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public void PrintPerson()
+    {
+        Console.WriteLine("이름: " + this.name);
+        Console.WriteLine("나이: " + this.age);
+        Console.WriteLine("성별: " + this.gender);
+    }
+}
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Person y = new Person("영희", 20, "여");
+        y.PrintPerson();
+
+        Person c = new Person("남", 20, "철수");
+        c.PrintPerson();
+    }
+}
+
