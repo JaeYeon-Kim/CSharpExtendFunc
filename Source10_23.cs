3c72996bb9c73a876595e45982e22b7c3fcd9087
JaeYoun Kim <rlawodusx@naver.com>
2024-08-16 18:13:19 +0900

    Feat: 정적 메서드 호출

---
+++ Source10_23.cs
@@ -0,0 +1,55 @@
+// /*
+// 정적 메서드 호출 
+// 클래스명.메서드명 형식으로 호출 
+// */
+
+// using System;
+
+// public class Circle
+// {
+//     public Circle(double radius)
+//     {
+//         Radius = radius;
+//     }
+//     public double Radius { get; set; }
+
+//     public double Diameter
+//     {
+//         get
+//         {
+//             return 2 * Radius;
+//         }
+//     }
+
+//     public double Circumference
+//     {
+//         get
+//         {
+//             return 2 * Radius * Math.PI;
+//         }
+//     }
+
+//     public double Area
+//     {
+//         get
+//         {
+//             return Math.PI * Math.Pow(Radius, 2);
+//         }
+//     }
+
+// }
+
+
+
+
+
+class MainClass
+{
+    public static void Main()
+    {
+        Circle c = new Circle(3);
+        Console.WriteLine(c.Diameter);
+        Console.WriteLine(c.Circumference);
+        Console.WriteLine(c.Area);
+    }
+}
