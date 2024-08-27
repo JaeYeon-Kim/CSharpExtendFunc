02e24e41a433cf6c14a8ad7169f89b1400fea716
JaeYoun Kim <rlawodusx@naver.com>
2024-08-27 18:47:25 +0900

    Feat: Enum & Switch

--- Test1_2.cs
+++ Test1_2.cs
@@ -5,9 +5,25 @@ namespace Test1_2
 
     class Program2
     {
-        static void Main(string[] args)
+
+        public enum Color { Red, Green, Blue };
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Color c = (Color)(new Random()).Next(0, 3);
+            switch (c)
+            {
+                case Color.Red:
+                Console.WriteLine("The Color is Red");
+                break;
+
+                case Color.Green:
+                Console.WriteLine("The Color is Green");
+                break;
+
+                case Color.Blue:
+                Console.WriteLine("The Color is Blue");
+                break;
+            }
         }
     }
 }
