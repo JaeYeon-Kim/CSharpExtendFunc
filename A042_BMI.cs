8cfc716508ac04718cbfc60f7557257b303801d2
JaeYoun Kim <rlawodusx@naver.com>
2024-08-31 23:52:40 +0900

    Feat: BMI Calculator

---
+++ A042_BMI.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace A042_BMI
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("키를 입력하세요(cm) : ");
+            
+        }
+    }
+}
