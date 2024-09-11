8a9741d37e024745ae799d1ff5bc14f2a1a3b302
JaeYoun Kim <rlawodusx@naver.com>
2024-09-11 19:29:59 +0900

    Feat: 코딩테스트 Start

--- PrimeNumberCount.cs
+++ PrimeNumberCount.cs
@@ -1,24 +1,24 @@
-class PrimeNumberCount
-{
-    static void Main(string[] args)
-    {
-        int primeNumberCount = 0;
-        string inputText = Console.ReadLine();
-        var number = int.Parse(inputText);
+// class PrimeNumberCount
+// {
+//     static void Main(string[] args)
+//     {
+//         int primeNumberCount = 0;
+//         string inputText = Console.ReadLine();
+//         var number = int.Parse(inputText);
 
-        for (int i = 2; i < number; i++)
-        {
-            if (i == 2)
-            {
-                primeNumberCount++;
-            }
-            else if (i % 2 != 0)
-            {
-                primeNumberCount++;
-            }
-        }
+//         for (int i = 2; i < number; i++)
+//         {
+//             if (i == 2)
+//             {
+//                 primeNumberCount++;
+//             }
+//             else if (i % 2 != 0)
+//             {
+//                 primeNumberCount++;
+//             }
+//         }
 
-        Console.WriteLine($"약수의 개수는? {primeNumberCount}");
+//         Console.WriteLine($"약수의 개수는? {primeNumberCount}");
 
-    }
-}
+//     }
+// }
--- codingTest.cs
+++ codingTest.cs
@@ -1,31 +1,22 @@
 
 /*
-자연수 n이 매개변수로 주어집니다. n을 x로 나눈 나머지가 1이 되도록 하는 가장 작은 자연수 x를 return 하도록 solution 함수를 완성해주세요. 
-답이 항상 존재함은 증명될 수 있습니다.
+함수 solution은 정수 n을 매개변수로 입력받습니다. n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요. 
+예를들어 n이 118372면 873211을 리턴하면 됩니다.
 */
+
+using System;
+using System.Collections.Generic;
 
 class CodingTest
 {
     static void Main(string[] args)
     {
-        Solution solve = new Solution();
-        solve.solution(20);
-    }
-}
-
-public class Solution
-{
-    public string solution(int num)
-    {
-        string answer = "";
-        if (num % 2 == 0)
+        List<int> newList = new List<int>();
+        newList.Add(1);
+        newList.Add(2);
+        for (int i = 0; i < 2; i++)
         {
-            answer = "Even"
+            Console.Write(newList[i]);
         }
-        else
-        {
-            answer = "Odd"
-        }
-        return answer;
     }
 }
