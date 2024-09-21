9ab98e41d561926e5edbb6f0c13ad0030d3814ae
JaeYoun Kim <rlawodusx@naver.com>
2024-09-21 18:20:14 +0900

    Feat: 평균 이상의 학생수 구하기

---
+++ AverageCount.cs
@@ -0,0 +1,36 @@
+/*
+평균 이상 학생수 구하기 
+//[?] 전체 학생 평균 이상의 성적인 학생의 수를 구하라 
+*/
+
+
+// class AverageCount
+// {
+//     static void Main(string[] args)
+//     {
+//         int[] scores = { 100, 90, 80, 70, 60 };
+//         int studentCount = 0;       // 학생 수 
+//         int totalScore = 0;
+
+//         foreach (var score in scores)
+//         {
+//             totalScore += score;
+//         }
+
+//         double averageScore = totalScore / scores.Length;
+
+//         Console.WriteLine($"총 스코어 및 평균 알려줘!! {totalScore} {averageScore}");
+
+//         foreach (var score in scores)
+//         {
+//             if (score >= averageScore)
+//             {
+//                 studentCount++;
+//             }
+//         }
+
+//         Console.WriteLine($"전체 학생 평균 이상의 성적인 학생의 수 {studentCount}");
+
+
+//     }
+// }
