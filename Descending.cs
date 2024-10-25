3e849a129423de18ac051c0c99194fa8bcf2a82e
JaeYoun Kim <rlawodusx@naver.com>
2024-10-25 21:58:48 +0900

    Feat: 코딩테스트
    행렬의덧셈

--- Descending.cs
+++ Descending.cs
@@ -1,25 +1,25 @@
-using System.ServiceModel;
-using System.ServiceModel.Channels;
+// using System.ServiceModel;
+// using System.ServiceModel.Channels;
 
-class Descending
-{
-    static void Main(string[] args)
-    {
-        Solution solve = new Solution();
-        solve.solution("zbcdefg");
-    }
-}
+// class Descending
+// {
+//     static void Main(string[] args)
+//     {
+//         Solution solve = new Solution();
+//         solve.solution("zbcdefg");
+//     }
+// }
 
-public class Solution
-{
-    public void solution(string s)
-    {
-        List<int> numberList = new List<int>();
-        char[] characters = s.ToCharArray();
+// public class Solution
+// {
+//     public void solution(string s)
+//     {
+//         List<int> numberList = new List<int>();
+//         char[] characters = s.ToCharArray();
 
-        foreach (char c in characters)
-        {
-            Console.WriteLine(c);   
-        }
-    }
-}
+//         foreach (char c in characters)
+//         {
+//             Console.WriteLine(c);   
+//         }
+//     }
+// }
