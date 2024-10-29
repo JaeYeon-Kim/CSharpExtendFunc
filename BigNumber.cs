f3cb818868317398fd42b9fb898f67611603bd7d
JaeYoun Kim <rlawodusx@naver.com>
2024-10-29 16:07:10 +0900

    Feat: 코딩테스트 문제
    약수의 합

--- BigNumber.cs
+++ BigNumber.cs
@@ -1,39 +1,39 @@
-using System;
+// using System;
 
-namespace BigNumber
-{
-    class CodingTest
-    {
-        static void Main(string[] args)
-        {
-            Solution solve = new Solution();
-            int result = solve.solution(15);
-            Console.WriteLine(result);
-        }
-    }
-
-
+// namespace BigNumber
+// {
+//     class CodingTest
+//     {
+//         static void Main(string[] args)
+//         {
+//             Solution solve = new Solution();
+//             int result = solve.solution(15);
+//             Console.WriteLine(result);
+//         }
+//     }
 
 
-    class Solution
-    {
-        public int solution(int n)
-        {
-            int answer = n + 1;
-            int countOne = ConvertToBinaryNumber(n);
-            while (ConvertToBinaryNumber(answer) != countOne)
-            {
-                answer++;
-            }
 
-            return answer;
-        }
 
-        public int ConvertToBinaryNumber(int number)
-        {
-            string binaryNumber = Convert.ToString(number, 2);
-            int countOne = binaryNumber.Split('1').Length - 1;
-            return countOne;
-        }
-    }
-}
+//     class Solution
+//     {
+//         public int solution(int n)
+//         {
+//             int answer = n + 1;
+//             int countOne = ConvertToBinaryNumber(n);
+//             while (ConvertToBinaryNumber(answer) != countOne)
+//             {
+//                 answer++;
+//             }
+
+//             return answer;
+//         }
+
+//         public int ConvertToBinaryNumber(int number)
+//         {
+//             string binaryNumber = Convert.ToString(number, 2);
+//             int countOne = binaryNumber.Split('1').Length - 1;
+//             return countOne;
+//         }
+//     }
+// }
