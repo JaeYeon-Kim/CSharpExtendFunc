72226285d542e5f40fab4f427c48b826a30826b5
JaeYoun Kim <rlawodusx@naver.com>
2024-10-30 17:38:02 +0900

    Feat: 코딩테스트
    문제 : 두개 뽑아서 더하기

--- SumOfMeasure.cs
+++ SumOfMeasure.cs
@@ -1,30 +1,30 @@
-namespace CodingTest
-{
-    class SumOfMeasure
-    {
-        static void Main(string[] args)
-        {
-           Solution solve = new Solution();
-           var result = solve.solution(5);
-           Console.WriteLine("약수의 합은? " + result);
-        }
-    }
+// namespace CodingTest
+// {
+//     class SumOfMeasure
+//     {
+//         static void Main(string[] args)
+//         {
+//            Solution solve = new Solution();
+//            var result = solve.solution(5);
+//            Console.WriteLine("약수의 합은? " + result);
+//         }
+//     }
 
-    public class Solution
-    {
-        public int solution(int n)
-        {
-            // 약수의 합 
-            int answer = 0;
+//     public class Solution
+//     {
+//         public int solution(int n)
+//         {
+//             // 약수의 합 
+//             int answer = 0;
 
-            for (int i = 1; i <= n; i++)
-            {
-                if (n % i == 0)
-                {
-                    answer += i;
-                }
-            }
-            return answer;
-        }
-    }
-}
+//             for (int i = 1; i <= n; i++)
+//             {
+//                 if (n % i == 0)
+//                 {
+//                     answer += i;
+//                 }
+//             }
+//             return answer;
+//         }
+//     }
+// }
---
+++ TwoAndSum.cs
@@ -0,0 +1,44 @@
+using System.Globalization;
+using System;
+using System.Linq;
+
+namespace CodingTest
+{
+    class TwoAndSum
+    {
+        static void Main(string[] args)
+        {
+            Solution solve = new Solution();
+            int[] result = [5, 0, 2, 7];
+            int[] resultArray = solve.solution(result);
+            foreach (var item in resultArray)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+
+
+    public class Solution
+    {
+        public int[] solution(int[] numbers)
+        {
+            List<int> answer = new List<int>();
+
+            int resultIndex = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    answer.Add(numbers[i] + numbers[j]);
+                    resultIndex++;
+                }
+            }
+
+            answer.Sort();
+
+            return answer.Distinct().ToArray();
+        }
+    }
+}
