a71cbaaaac9cf0b58eec939dddbac1fa4ba314c7
JaeYoun Kim <rlawodusx@naver.com>
2024-10-31 17:16:56 +0900

    Feat: 코딩테스트 - 가위바위보

--- TwoAndSum.cs
+++ TwoAndSum.cs
@@ -1,44 +1,44 @@
-using System.Globalization;
-using System;
-using System.Linq;
+// using System.Globalization;
+// using System;
+// using System.Linq;
 
-namespace CodingTest
-{
-    class TwoAndSum
-    {
-        static void Main(string[] args)
-        {
-            Solution solve = new Solution();
-            int[] result = [5, 0, 2, 7];
-            int[] resultArray = solve.solution(result);
-            foreach (var item in resultArray)
-            {
-                Console.Write(item + " ");
-            }
-        }
-    }
+// namespace CodingTest
+// {
+//     class TwoAndSum
+//     {
+//         static void Main(string[] args)
+//         {
+//             Solution solve = new Solution();
+//             int[] result = [5, 0, 2, 7];
+//             int[] resultArray = solve.solution(result);
+//             foreach (var item in resultArray)
+//             {
+//                 Console.Write(item + " ");
+//             }
+//         }
+//     }
 
 
-    public class Solution
-    {
-        public int[] solution(int[] numbers)
-        {
-            List<int> answer = new List<int>();
+//     public class Solution
+//     {
+//         public int[] solution(int[] numbers)
+//         {
+//             List<int> answer = new List<int>();
 
-            int resultIndex = 0;
+//             int resultIndex = 0;
 
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                for (int j = i + 1; j < numbers.Length; j++)
-                {
-                    answer.Add(numbers[i] + numbers[j]);
-                    resultIndex++;
-                }
-            }
+//             for (int i = 0; i < numbers.Length; i++)
+//             {
+//                 for (int j = i + 1; j < numbers.Length; j++)
+//                 {
+//                     answer.Add(numbers[i] + numbers[j]);
+//                     resultIndex++;
+//                 }
+//             }
 
-            answer.Sort();
+//             answer.Sort();
 
-            return answer.Distinct().ToArray();
-        }
-    }
-}
+//             return answer.Distinct().ToArray();
+//         }
+//     }
+// }
